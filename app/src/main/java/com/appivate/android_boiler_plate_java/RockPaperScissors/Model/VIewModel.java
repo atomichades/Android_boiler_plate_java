75636cf0cd10ab7c6586a16c015bac27c523942d
atomichades@gmail.com <Delta3231155*>
2022-02-04 23:24:56 +0000

    Initial commit

---
+++ app/src/main/java/com/appivate/android_boiler_plate_java/RockPaperScissors/Model/VIewModel.java
@@ -0,0 +1,4 @@
+package com.appivate.android_boiler_plate_java.RockPaperScissors.Model;
+
+public class VIewModel {
+}
---
+++ app/src/main/java/com/appivate/android_boiler_plate_java/RockPaperScissors/ViewController/MainActivity.java
@@ -0,0 +1,15 @@
+package com.appivate.android_boiler_plate_java.RockPaperScissors.ViewController;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.appivate.android_boiler_plate_java.R;
+
+public class MainActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_layout);
+    }
+}
